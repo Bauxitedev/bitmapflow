5f1314771d5b8b9bb7b314b2a9436738619e1504
Bauxitedev <31383123+Bauxitedev@users.noreply.github.com>
2021-03-30 13:21:57 +0200

    Fix NaN numbox bug crashing the program on Linux if you drag the FPS numbox

--- rust/src/global_holder.rs
+++ rust/src/global_holder.rs
@@ -2,14 +2,24 @@ use gdnative::prelude::*;
 
 #[derive(NativeClass, Default)]
 #[inherit(Node)]
+#[register_with(Self::register)]
 pub struct GlobalHolder {
-    #[property(path = "fps")]
     pub fps: f64,
 }
 
 impl GlobalHolder {
     fn new(_owner: &Node) -> Self {
         GlobalHolder::default()
+    }
+
+    fn register(builder: &ClassBuilder<Self>) {
+        builder
+            .add_property("fps")
+            .with_setter(|this, _owner, value: f64| {
+                this.fps = if value.is_finite() { value } else { 10. }
+            })
+            .with_getter(|this, _owner| this.fps)
+            .done();
     }
 }
 
